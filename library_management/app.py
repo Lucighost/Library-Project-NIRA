6dd6fa374424022702d59f3a72cf69c5bb02983e
Sivaram Gopi <you@example.com>
2024-11-25 01:17:32 +0530

    second commit

--- library_management/app.py
+++ library_management/app.py
@@ -5,3 +5,14 @@ def hello_world():
     return 'Hello, Library Management System!'
 if ('__nira__') == '__main__':
     app.run(debug=True)
+
+    books = []
+class Book:
+    def __init__(self, id, title, author, year):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def to_dict(self):
+        return {"id": self.id, "title": self.title, "author": self.author, "year": self.year}
